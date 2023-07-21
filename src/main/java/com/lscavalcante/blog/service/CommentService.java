a956cf24a5caaee2b9f294447644963068d65331
lucascavalcante <lucasantoscv@gmail.com>
2023-07-21 17:19:51 +0100

    feature: comment service and configs

--- src/main/java/com/lscavalcante/blog/service/CommentService.java
+++ src/main/java/com/lscavalcante/blog/service/CommentService.java
@@ -2,12 +2,16 @@ package com.lscavalcante.blog.service;
 
 import com.lscavalcante.blog.configuration.exception.RecordException;
 import com.lscavalcante.blog.configuration.exception.UnMappedException;
+import com.lscavalcante.blog.configuration.security.SecurityConfiguration;
 import com.lscavalcante.blog.dto.comment.RequestCreateComment;
 import com.lscavalcante.blog.dto.comment.RequestUpdateComment;
 import com.lscavalcante.blog.dto.comment.ResponseCommentDetail;
 import com.lscavalcante.blog.model.blog.Blog;
 import com.lscavalcante.blog.model.comment.Comment;
+import com.lscavalcante.blog.model.user.User;
 import com.lscavalcante.blog.repository.CommentRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -23,11 +27,15 @@ public class CommentService {
 
     public ResponseCommentDetail createComment(Long blogId, RequestCreateComment request) {
         try{
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            User user = (User) authentication.getPrincipal();
+
             Blog blog = blogService.findBlogById(blogId, true);
 
             Comment comment = new Comment();
             comment.setBlog(blog);
             comment.setContent(request.getContent());
+            comment.setAuthor(user);
             commentRepository.save(comment);
 
             return modelMapperService.map(comment, ResponseCommentDetail.class);
@@ -40,8 +48,20 @@ public class CommentService {
 
     public ResponseCommentDetail updateComment(Long commentId, Long blogId, RequestUpdateComment request) {
         try{
+
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            User user = (User) authentication.getPrincipal();
+
             Comment  comment = commentRepository.findById(commentId).orElseThrow(() -> new RecordException("Comment not found"));
             blogService.findBlogById(blogId, true);
+
+            // rules before the update
+
+            if(!comment.getAuthor().getId().equals(user.getId())) {
+                throw new RuntimeException("You aren't authorized to update this comment, you aren't the author");
+            }
+
+            // update the blog
 
             modelMapperService.map(request, comment);
             commentRepository.save(comment);
