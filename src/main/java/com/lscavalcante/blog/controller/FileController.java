9e080a19961d7f1907558ad40b05458fc2e46f00
lucascavalcante <lucasantoscv@gmail.com>
2023-07-21 17:23:43 +0100

    doc: endpoint for download files

---
+++ src/main/java/com/lscavalcante/blog/controller/FileController.java
@@ -0,0 +1,37 @@
+package com.lscavalcante.blog.controller;
+
+import com.lscavalcante.blog.service.UploadService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+
+@Tag(name = "Files")
+@RestController
+@RequestMapping("files")
+public class FileController {
+    private final UploadService uploadService;
+
+    public FileController(UploadService uploadService) {
+        this.uploadService = uploadService;
+    }
+
+    @GetMapping("{filename:.+}")
+    public ResponseEntity<Resource> download(@PathVariable String filename, HttpServletRequest httpServletRequest) throws IOException {
+        Resource resource = uploadService.show("uploads", filename);
+        String contentyType = "";
+        contentyType = httpServletRequest.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+
+        if (contentyType.isBlank()) contentyType = "application/octet-stream";
+
+        return ResponseEntity.ok().contentType(MediaType.parseMediaType(contentyType)).body(resource);
+    }
+}
